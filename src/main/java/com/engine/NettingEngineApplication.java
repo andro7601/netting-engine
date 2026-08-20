f6ceb336aab5134042d41f9215b45ea01456f6a7
Andria Shvelidze <shvelidze2007@gmail.com>
2026-08-20 16:03:37 +0400

    initial commit

---
+++ src/main/java/com/engine/NettingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engine/NettingEngineApplicationTests.java
@@ -0,0 +1,15 @@
+package com.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class NettingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
